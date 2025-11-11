5d7f2099440d0b316cdd7fa2e31cd8ef5743ab2e
pvieira- <pablomatemat@gmail.com>
2025-11-11 17:24:04 -0300

    Vetor soma

--- 10_Vetores/op_vet.py
+++ 10_Vetores/op_vet.py
@@ -2,18 +2,18 @@ def print_vet(V :list[float]) -> None:
     print(V)
 
 def soma_vet(V1 :list[float], V2 :list[float]) -> list[float]:
-    ln = len(V1)
-#    if ln != len(V2):
-#        return (False)
-
-    VF = []
+    ln :int = len(V1)
+    VF :list[float] = []
     for i in range(ln):
         VF.append(V1[i] + V2[i])
     return(VF)
 
+def subtracao_vet(V1 :list[float], V2 :list[float]) -> list[float]:
+    pass
+
 if __name__ == '__main__':
-    VA = [1 ,3, 8]
-    VB = [-3, 4]
+    VA = [1 ,3, 8, 9]
+    VB = [-3, 4, 0, -1]
 
     print_vet(VA)
     print_vet(VB)
