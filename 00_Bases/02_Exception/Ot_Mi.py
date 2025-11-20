4739d7d041ea04f22683e5d9b7dcbc44d0320420
pvieira- <pablomatemat@gmail.com>
2025-11-20 14:34:06 -0300

    Pasta Exception Ot_Mi.py

--- 00_Bases/02_Exception/Ot_Mi.py
+++ 00_Bases/02_Exception/Ot_Mi.py
@@ -1,12 +1,28 @@
-a = 0 
+import traceback
+import sys
+
+def ll():
+    try:
+        lis = [1, 2, 3]
+        for i in range(len(lis) + 1):
+            print(f'passei na ll() lis -> {lis[i]}')
+    except IndexError as err:
+        print(f'ERROOOO: {err}')
+    print('Dentro de ll() e depois do execpt')
+
+a = int(sys.argv[1])
 try:
     b = 1/a
 
     print(f'b = {b}')
 
-except Exception as erro:
+except ZeroDivisionError as erro:
     a = 4
     print('ERROR:', erro)
+#    trace = traceback.format_exc()
+#    print(trace)
+    ll()
+    print('Apos ll()')
 
 else:
     print('To no else.')
@@ -18,11 +34,3 @@ finally:
 print(f'Eita porra a = {a}')
 
 
-
-#while (a == True):
-#    se = input('Insira a senha: ')
-#    if se == 'ab':
-#        print('Login feito')
-#        a = False
-#    else:
-#        print('senha incorreta')
