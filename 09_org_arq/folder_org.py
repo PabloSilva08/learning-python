872c77431e493f0f9ae478c2177b5cc0ff90c6ee
pvieira- <pablomatemat@gmail.com>
2025-12-03 17:22:23 -0300

    Aperfeiçoando o path do 09_org_arq/folder_org.py

--- 09_org_arq/folder_org.py
+++ 09_org_arq/folder_org.py
@@ -22,7 +22,45 @@ def mv_arq(path_main, folder, target):
     print(f'{target} -> {folder}')
     os.rename(path_old, path_new)
 
-    
+def ft_menu() -> str:
+    menu = """
+###############################################################################
+    Você gostaria de:
+    1 - Organizar o Desktop.
+    2 - Organizar o Dowload.
+    3 - Digitar o caminho do diretório.
+    Ou digite qualquer coisa para sair.
+##############################################################################
+   >> """
+    return(input(menu))
+
+def ft_constructor_path(path_main: str) -> str:
+    option = ft_menu()
+    if option == '1':
+        if os.path.isdir(os.path.join(path_main, 'Desktop')):
+            print('Desk')
+        else:
+            path_main = os.path.join(path_main, 'Área de trabalho')
+            if (os.path.isdir(path_main)):
+                print('oooooooooooooooooooooook')
+#        print(path_main)
+    elif option == '2':
+        path_main = os.path.join(path_main, 'Downloads')
+ #       print(path_main)
+    elif option == '3':
+        partes = input("Digite o caminho a partir do home/: ")
+        partes = partes.split(os.sep)
+        for parte in partes:
+            path_main = os.path.join(path_main, parte)
+#        print(path_main)
+    elif option == 'diretorio atual':
+        path_main = os.path.abspath('.') #Para teste, retirar depois
+#        print(path_main)
+    else:
+        print(':D')
+        return
+    return(path_main)
+
 def main() -> int:
     list_folder ={
         'Documentos':['.pdf', '.txt', '.odt', '.doc', '.docx', '.ppt', '.pptx'],
@@ -32,16 +70,19 @@ def main() -> int:
         'Code':['.py', '.html', '.c', '.c++', '.js', '.css'],
         'Compact':['.zip', '.rar', '.gz', '.7z', '.xz', '.bz2']
     }
-#    path_main = '/home/pablo/Downloads'
-    path_main = os.path.abspath('.') #Para teste, retirar depois
-    target_string = os.listdir(path_main)
+    path_main = os.path.expanduser('~')
+    path_main = ft_constructor_path(path_main)
+    print(f'O path_main é: {path_main}')
 
-    for target in target_string:
-        if os.path.isdir(os.path.join(path_main,target)):
-           continue 
-        extension = str.lower(capture_extension(target))
-        folder = create_folder(path_main, extension, list_folder)
-        mv_arq(path_main, folder, target)
+    target_string = os.listdir(path_main)
+#    print(target_string)
+
+#    for target in target_string:
+#        if os.path.isdir(os.path.join(path_main,target)):
+#           continue 
+#        extension = str.lower(capture_extension(target))
+#        folder = create_folder(path_main, extension, list_folder)
+#        mv_arq(path_main, folder, target)
     return(0)
 
 if '__main__' == __name__:
