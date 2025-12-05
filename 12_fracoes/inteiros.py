4d7f69314b7f150220087ae213a23670ba4d4314
pvieira- <pablomatemat@gmail.com>
2025-12-05 14:38:53 -0300

    Fatores primos

--- 12_fracoes/inteiros.py
+++ 12_fracoes/inteiros.py
@@ -1,17 +1,22 @@
 import sys
 
-def fator_primo(num: int) -> list[int]:
-    ft = []
+def fatores_primo(num: int) -> list[int]:
+    fatores_primos = []
     if num == 1:
         return(1)
     if num < 1:
         return(0)
-    for i in range(2, num):
-        if ((num % i) == 0):
-            ft.append(i)
-            num = num / i
+    n = num
+    i = 2
+    while (i <= num):
+        if ((n % i) == 0):
+            fatores_primos.append(i)
+            n = n / i
             continue
+        i += 1
+    return(fatores_primos)
 
 if '__main__' == __name__:
-    num = sys.argv[1]
-    fator = ft_fator_primo(num)
+    num = int(sys.argv[1])
+    fatores = fatores_primo(num)
+    print(fatores)
